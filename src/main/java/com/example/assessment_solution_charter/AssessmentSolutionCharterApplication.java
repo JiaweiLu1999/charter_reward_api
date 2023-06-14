5bd1f66400476404805120f326e20f1d214b6511
Jiawei Lu <jl5999@columbia.edu>
2023-06-14 16:07:49 -0400

    init commit

---
+++ src/main/java/com/example/assessment_solution_charter/AssessmentSolutionCharterApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessment_solution_charter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentSolutionCharterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentSolutionCharterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assessment_solution_charter/AssessmentSolutionCharterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessment_solution_charter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentSolutionCharterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
